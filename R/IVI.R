7d6c53d9d5adaa4eee646ff9df071c1eff08d442
fahrihasby <fahri.hasby@google.com>
2024-09-14 23:48:47 +0200

    Initial commit

---
+++ R/IVI.R
@@ -0,0 +1,21 @@
+#' Analyze IVI from DBH data
+#'
+#' @param data a data frame or tibble that contains subplot, species, and diamaeter at breast height (DBH, in cm)
+#' @param plot.size The plot size in m^2. The default value is 100 m^2.
+#' @param n.subplot The number of subplot. The default value is 5.
+#' @returns a tibble containing species name, relative frequency, relative density, relative coverage, and importance value index.
+#' @import dplyr
+#' @export
+
+IVI <- function(data, plot.size = 100, n.subplot=5){
+  result <- data %>%
+    group_by(Species) %>%
+    mutate(LAB = pi*(0.5*DBH)^2) %>%
+    summarise(Freq_rf = n_distinct(Subplot)/n.subplot,
+              Kr_rf = n()/100,
+              Kb_rf = sum(LAB)) %>%
+    mutate_if(is.numeric,.funs = ~./sum(.)*100) %>%
+    mutate(IVI = Freq_rf + Kr_rf + Kb_rf) %>%
+    arrange(desc(IVI))
+  print(result)
+}
