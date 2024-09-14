c68085ac293f984befd7343e1bc5f776fc0739de
fahrihasby <fahri.hasby@google.com>
2024-09-15 00:33:54 +0200

    Modification for better documentation.

--- R/IVI.R
+++ R/IVI.R
@@ -1,12 +1,15 @@
 #' Analyze IVI from DBH data
 #'
-#' @param data a data frame or tibble that contains subplot, species, and diamaeter at breast height (DBH, in cm)
+#' Importance value index (IVI) measures how dominant a species is in a given forest area.
+#'
+#' @param data a data frame or tibble that contains subplot, species, and diamaeter at breast height (DBH, in cm).
 #' @param plot.size The plot size in m^2. The default value is 100 m^2.
 #' @param n.subplot The number of subplot. The default value is 5.
 #' @returns a tibble containing species name, relative frequency, relative density, relative coverage, and importance value index.
 #' @import dplyr
 #' @export
-
+#'
+#' @examples
 IVI <- function(data, plot.size = 100, n.subplot=5){
   result <- data %>%
     group_by(Species) %>%
@@ -17,5 +20,4 @@ IVI <- function(data, plot.size = 100, n.subplot=5){
     mutate_if(is.numeric,.funs = ~./sum(.)*100) %>%
     mutate(IVI = Freq_rf + Kr_rf + Kb_rf) %>%
     arrange(desc(IVI))
-  print(result)
 }
