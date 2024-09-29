d5c1b35bd132c7ddfb9deb17176ba4f80f49de22
fahrihasby <fahri.hasby@google.com>
2024-09-29 09:41:50 +0200

    Add edaphic data

---
+++ R/Edaphic_data.R
@@ -0,0 +1,20 @@
+#' Measurements of soil physical properties with gravimetric methods
+#'
+#' A subset of soil physical properties with gravimetric method from Mount Papandayan, West Java, Indonesia.
+#'
+#' @format ## `Edaphic_data`
+#' A data frame with 10 rows and 10 columns:
+#' \describe{
+#'   \item{Plot}{Plot number}
+#'   \item{T}{Soil temperature}
+#'   \item{pH}{Soil pH}
+#'   \item{Rh}{Soil relative humidity}
+#'   \item{Core_Wet}{Soil wet weight for bulk density measurement with core sampler method in gram}
+#'   \item{Core_Dry}{Soil dry weight for bulk density measurement with core sampler method in gram}
+#'   \item{Cru_Dry}{Soil dry weight from auger bore plus crucible in gram}
+#'   \item{Cru_Ash}{Ash weight after furnace from auger bore plus crucible in gram}
+#'   \item{Au_Ash}{Ash weight after furnace from auger bore in gram}
+#'   \item{Crucible}{Weight of the crucibles in gram}
+#' }
+#' @source Noviana Budianti private repo
+"Edaphic_data"
